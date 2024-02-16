7d820d8c9208f65f2eb2274cfc0d73f418c2f7f9
choi09 <0930alan@gmail.com>
2024-02-16 17:18:20 +0900

    Feat: 동화책 수정 #11

--- src/main/java/com/spring/sorigalpi/controller/BookController.java
+++ src/main/java/com/spring/sorigalpi/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -91,6 +92,10 @@ public class BookController {
 		return new ResponseEntity<>(basicResponse, HttpStatus.OK);
 	}
 	
+	@PutMapping("updateBook")
+	public void updateBook() { //동화책 수정
+		
+	}
 	
 	@Data
 	@Builder
