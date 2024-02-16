8106c5c475840743943c15d5b9c925cf31d6f0d5
RyuSeohyeong <ryugowell@gmail.com>
2024-02-16 16:58:10 +0900

    Test

--- src/main/java/com/spring/sorigalpi/controller/TestController.java
+++ src/main/java/com/spring/sorigalpi/controller/TestController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "test")
  @RestController
 public class TestController {
 
+	@ApiOperation(value="테스트", notes="테스트")
 	@GetMapping("/test")
 	public String test() {
 		return "Hello World!";
