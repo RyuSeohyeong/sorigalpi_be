859926125180547533d4e726c43d6d56d87340bd
RyuSeohyeong <ryugowell@gmail.com>
2024-04-02 01:09:41 +0900

    Refactoring: Member 인스턴스에 Optional 타입 부여

--- src/main/java/com/spring/sorigalpi/repository/MemberRepository.java
+++ src/main/java/com/spring/sorigalpi/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package com.spring.sorigalpi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.sorigalpi.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     
-	Member findByEmail(String email);
+	  Optional<Member> findByEmail(String email);
 
 }
