58606a75af26e6b9ce36b61abe23e8dae451dec0
RyuSeohyeong <ryugowell@gmail.com>
2024-04-05 00:06:08 +0900

    Feat: JWT Authentication(인증), Authorization(인가) 및 인증된 사용자 로그인 구현 #37

--- src/main/java/com/spring/sorigalpi/controller/MemberController.java
+++ src/main/java/com/spring/sorigalpi/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.spring.sorigalpi.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,59 +17,76 @@ import com.spring.sorigalpi.dto.MemberDto;
 import com.spring.sorigalpi.dto.MemberLoginDto;
 import com.spring.sorigalpi.entity.Member;
 import com.spring.sorigalpi.service.MemberService;
+import com.spring.sorigalpi.token.PrincipalDetails;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
-@Api(tags="member")
+@Api(tags = "member")
 @RestController("memberController")
 @RequestMapping(value = "/member")
 @RequiredArgsConstructor
 
 public class MemberController {
-	
-    private final MemberService memberService;
-    
-  
-	@ApiOperation(value="회원 가입", notes="회원 가입")
+
+	private final MemberService memberService;
+
+	@ApiOperation(value = "회원 가입", notes = "회원 가입")
 	@PostMapping("/signUp")
 	public Member createMember(@RequestBody MemberDto memberDto) {
-		
+
 		return memberService.createMember(memberDto);
 	}
-	
-	@ApiOperation(value="로그인", notes="로그인")
+
+	@ApiOperation(value = "로그인", notes = "로그인")
 	@PostMapping("/login")
 	public String login(@RequestBody MemberLoginDto memberLoginDto) {
-		
+
 		return memberService.login(memberLoginDto);
-    }
-	
-	
-	@ApiOperation(value="사용자 조회", notes="사용자 조회")
+	}
+
+	@ApiOperation(value = "사용자 조회", notes = "사용자 조회")
 	@GetMapping("/listMembers")
-	public List<Member> listeMembers(){
+	public List<Member> listeMembers() {
 		return (List<Member>) memberService.listMembers();
 	}
-	
-	@ApiOperation(value="사용자 정보 변경", notes="사용자 정보 변경")
+
+	@ApiOperation(value = "사용자 정보 변경", notes = "사용자 정보 변경")
 	@PutMapping("/{memberId}")
-		public String updatMember( @ApiParam(name = "memberId", value = "사용자 고유 아이디", required = true) @PathVariable String memberId, @RequestBody MemberDto memberDto) {
-			return memberService.updateMember(memberId, memberDto);
-		}
-	
-	@ApiOperation(value="사용자 탈퇴", notes="사용자 탈퇴")
+	public String updatMember(
+			@ApiParam(name = "memberId", value = "사용자 고유 아이디", required = true) @PathVariable String memberId,
+			@RequestBody MemberDto memberDto) {
+		return memberService.updateMember(memberId, memberDto);
+	}
+
+	@ApiOperation(value = "사용자 탈퇴", notes = "사용자 탈퇴")
 	@DeleteMapping("/{memberId}")
 	public String deleteMember(
-			@ApiParam(name = "memberId", value = "사용자 고유 아이디", required = true)
-			@PathVariable String memberId) {
+			@ApiParam(name = "memberId", value = "사용자 고유 아이디", required = true) @PathVariable String memberId) {
 		return memberService.deleteMember(memberId);
 	}
-	
+
+	@ApiOperation(value = "사용자 email로 찾기", notes = "사용자 email로 찾기")
+
 	@GetMapping("/{email}")
-	public Member findMember (@PathVariable String email){
+	public Member findMember(@PathVariable String email) {
 		return memberService.findMember(email);
+	}
+
+	@ApiOperation(value = "사용자 JWT Token인증 확인", notes = "사용자 JWT Token인증 확인")
+	@GetMapping("/jwtTokenInfo")
+	public String info(@AuthenticationPrincipal PrincipalDetails principalDetails, Authentication authentication) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("PrincipalDetails "); // append메서드: 문자열 추가
+		sb.append(principalDetails);
+		sb.append("\n"); // \n 줄바꿈
+		sb.append("authentication ");
+		sb.append(authentication);
+
+		return sb.toString();
+
 	}
 }
---
+++ src/main/java/com/spring/sorigalpi/token/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.spring.sorigalpi.token;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.sorigalpi.entity.Member;
+
+import io.jsonwebtoken.io.IOException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private final AuthenticationManager authenticationManager;
+
+	private final JwtProvider jwtProvider;
+
+	// 인증(Authentication)객체
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+
+			ObjectMapper om = new ObjectMapper();
+			Member member = om.readValue(request.getInputStream(), Member.class);
+
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+					member.getEmail(), member.getPwd());
+
+			// SpringSecurity의 AuthenticationManager 인터페이스를 통 사용자의 자격 증명을
+			// 검증하고, 적절한 Authentication 객체를 반환합니다. (AuthenticationToken엔 사용자의 이메일과 비밀번호가
+			// 저장되어있다.
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+			return authentication;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+		String memberId = principalDetails.getMember().getMemberId();
+		String email = principalDetails.getMember().getEmail();
+
+		String jwtToken = jwtProvider.generateJwtToken(memberId, email);
+
+		response.addHeader("Authorization", "Bearer " + jwtToken);
+
+	}
+}
