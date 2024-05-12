4b1bee27d8bb589c127bd5ef2c9ec8d8ed67ae01
RyuSeohyeong <ryugowell@gmail.com>
2024-05-12 21:12:54 +0900

    Feat: 예외처리에 HttpStatus 401 추가 및 403 변경

--- src/main/java/com/spring/sorigalpi/exception/ErrorCode.java
+++ src/main/java/com/spring/sorigalpi/exception/ErrorCode.java
@@ -8,10 +8,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
+	
+	//401 Unauthorize 권한이 유효하지 않습니다.
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN_001", "유효하지 않은 토큰입니다."),
 
-	// 403 Forbidden 사용자가 콘텐츠에 접근할 권리를 가지고 있지 않습니다.
-	INVALID_TOKEN(HttpStatus.FORBIDDEN, "TOKEN_001", "만료된 토큰입니다."),
-	NO_AUTHORIZED(HttpStatus.FORBIDDEN, "AUTHORIZE_001", "인증되지 않았습니다."),
+	// 403 Forbidden 권한이 없습니다.
+	EXPIRED_TOKEN(HttpStatus.FORBIDDEN, "TOKEN_002", "만료된 토큰입니다."),
+	NO_AUTHORIZED(HttpStatus.FORBIDDEN, "AUTHORIZE_001", "권한이 없습니다."),
 
 	// 404 Not Found 서버가 요청 받은 리소스를 찾을 수 없음
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "존재하지 않는 이메일입니다.");
