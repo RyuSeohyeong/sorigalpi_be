9ca9fa74308451faf2991b744220740bcf741309
RyuSeohyeong <ryugowell@gmail.com>
2024-08-12 22:10:58 +0900

    Docs: 비밀번호 재설 링크 >> 비밀번호 재설정 링크로 변경

--- src/main/java/com/spring/sorigalpi/service/PwdMailService.java
+++ src/main/java/com/spring/sorigalpi/service/PwdMailService.java
@@ -23,7 +23,7 @@ public class PwdMailService {
 			
 			String emailContent = "<div>"
 		            + "<p>아래에 있는 링크를 클릭하면 비밀번호 재설정 페이지로 이동합니다.</p>"
-		            + "<a href='http://localhost:8080/member/find/pwd/" + verifyCode.getCode() + "'> 비밀번호 재설 링크 </a>"
+		            + "<a href='http://localhost:8080/member/find/pwd/" + verifyCode.getCode() + "'> 비밀번호 재설정 링크 </a>"
 		            + "</div>";
 
 			emailSenderService.sendEmail(verifyCode.getEmail(), "소리갈피 비밀번호 재설정 이메일 입니다.", emailContent); // 이메일 전송한다.
